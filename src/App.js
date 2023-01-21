a11f37a22b4c19c05d4da5b3a28591f363edb64a
J-David-Baxter <jdavidbx@gmail.com>
2023-01-21 15:46:04 -0500

    Build structure, add tachyon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import React from "react";
+import { useQuery, gql } from '@apollo/client';
+
+const GET_TODOS = gql`
+  query getTodos {
+    todos {
+      id
+      text
+      done
+    }
+  }
+`
 
 function App() {
+  const { data, loading, error } = useQuery(GET_TODOS);
+
+  if (loading) return <div>Loading todos...</div>
+  if (error) return <div>Error fetching todos</div>
+  
   return (
-    <div>App</div>
+    <div>
+      <h1>GraphQL Todo List</h1>
+      <form>
+        <input
+          type="text"
+          placeholder="Enter a todo"
+        />
+      </form>
+      <div>
+        {data.todos.map(todo => (
+          <p key={todo.id}>
+            <span>{todo.text}</span>
+            <button>&times;</button>
+          </p>
+        ))}
+      </div>
+    </div>
   );
 }
 
