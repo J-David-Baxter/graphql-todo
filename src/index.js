86f43330733f1299ab353f885797232c63d37240
J-David-Baxter <jdavidbx@gmail.com>
2023-01-21 15:00:34 -0500

    Connect to hasura with apollo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql, createHttpLink } from '@apollo/client';
+import {setContext} from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: 'https://react-todo-jdb.hasura.app/v1/graphql'
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = '8D1Th4jDKY7m4erOSEzl41vvG4wEZ8mJMao9lCt40evLUEu7gQC30wVp2n81DMth';
+  return {
+    headers: {
+      ...headers,
+      "x-hasura-admin-secret": token
+    }
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
+
+client.query({
+  query: gql`
+    query getTodos {
+      todos {
+        done
+        id
+        text
+      }
+    }
+  `
+}).then((data) => console.log(data));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
